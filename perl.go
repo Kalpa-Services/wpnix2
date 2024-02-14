26dd73d4f5a6f96bd3afda7ab62391be942300cf
carlhandy <carl@kalpa.dev>
2024-02-14 19:45:57 -0400

    update error handling in perl file

--- perl.go
+++ perl.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func checkAndInstallPerl() {
 	if _, err := exec.LookPath("perl"); err != nil {
 		fmt.Println("Perl is not installed. Installing Perl...")
-		exec.Command("apt", "install", "perl", "-y").Run()
+		if err := exec.Command("apt", "install", "perl", "-y").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError installing Perl:", err, "\x1b[0m")
+		} else {
+			fmt.Println("\x1b[32mPerl installed successfully.\x1b[0m")
+		}
 	} else {
-		fmt.Println("Perl is already installed.")
+		fmt.Println("\x1b[33mPerl is already installed.\x1b[0m")
 	}
 }
