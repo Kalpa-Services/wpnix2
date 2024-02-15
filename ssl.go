7655dd2b22951ddf9011b84c10ab1ce9cef5e89a
carlhandy <carl@kalpa.dev>
2024-02-14 22:04:24 -0400

    Revert "print certbot error message"
    This reverts commit 0b45696b360d6d865fb3e0359fa1ce23c72d9003.

---
+++ ssl.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func checkAndInstallCertbot() {
+	if _, err := exec.LookPath("certbot"); err != nil {
+		fmt.Println("\x1b[33mCertbot is not installed. Installing Certbot using Snap...\x1b[0m")
+		if err := exec.Command("apt", "update").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError updating package lists:", err, "\x1b[0m")
+			return
+		}
+		if err := exec.Command("apt", "install", "snapd", "-y").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError installing snapd:", err, "\x1b[0m")
+			return
+		}
+		if err := exec.Command("snap", "install", "core").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError installing snap core:", err, "\x1b[0m")
+			return
+		}
+		if err := exec.Command("snap", "refresh", "core").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError refreshing snap core:", err, "\x1b[0m")
+			return
+		}
+		if err := exec.Command("snap", "install", "--classic", "certbot").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError installing Certbot:", err, "\x1b[0m")
+			return
+		}
+		if err := exec.Command("ln", "-s", "/snap/bin/certbot", "/usr/bin/certbot").Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "\x1b[31mError creating symlink for Certbot:", err, "\x1b[0m")
+			return
+		}
+		fmt.Println("\x1b[32mCertbot installed successfully.\x1b[0m")
+	} else {
+		fmt.Println("\x1b[33mCertbot is already installed.\x1b[0m")
+	}
+}
+
+func configureLetsEncryptSSL(domain string) {
+	fmt.Println("Configuring Let's Encrypt SSL for", domain, "...")
+
+	var cmd *exec.Cmd
+	if strings.Count(domain, ".") > 1 {
+		cmd = exec.Command("certbot", "--nginx", "-d", domain)
+	} else {
+		cmd = exec.Command("certbot", "--nginx", "-d", domain, "-d", "www."+domain)
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, "\x1b[31mError configuring Let's Encrypt SSL for "+domain+":", err, "\x1b[0m")
+	} else {
+		fmt.Println("\x1b[32mSuccessfully configured Let's Encrypt SSL for", domain, "\x1b[0m")
+	}
+}
